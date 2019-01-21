443a9fe8f32286abe25266dc9b98b8ab5bc1a4e8
impolun <you@example.com[D[D[D[D[D[D[D[D[D[D[D[Dimpolun@gmail.com>
2019-01-21 13:46:00 +0300

    Fix KVService

--- src/main/java/ru/mail/polis/ivanov/KVServiceImpl.java
+++ src/main/java/ru/mail/polis/ivanov/KVServiceImpl.java
@@ -4,7 +4,6 @@ import one.nio.http.*;
 import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.KVDao;
 import ru.mail.polis.KVService;
-
 import java.io.IOException;
 
 public class KVServiceImpl extends HttpServer implements KVService {
@@ -15,64 +14,50 @@ public class KVServiceImpl extends HttpServer implements KVService {
     private final KVDao kvDao;
 
     public KVServiceImpl(@NotNull HttpServerConfig config, @NotNull KVDao kvDao) throws IOException {
-
         super(config);
         this.kvDao = kvDao;
-
     }
 
     @Path("/v0/status")
     public void status(Request request, HttpSession session) throws IOException {
 
         if (request.getMethod() == Request.METHOD_GET) {
-
             session.sendResponse(Response.ok(Response.EMPTY));
-
         } else {
-
             session.sendError(Response.BAD_REQUEST, null);
-
         }
-
     }
 
 
     @Path("/v0/entity")
     public void entity(Request request, HttpSession session) throws IOException {
 
-        if (request.getParameter(ID) == null || request.getParameter(ID).isEmpty()) {
+        String key = request.getParameter(ID);
 
+        if (key == null || key.isEmpty()) {
             session.sendError(Response.BAD_REQUEST, null);
-
+            return;
         }
 
         switch (request.getMethod()) {
 
-            case Request.METHOD_GET: {
+            case Request.METHOD_GET:
+                session.sendResponse(new Response(Response.OK, kvDao.get(key.getBytes())));
+                break;
 
-                session.sendResponse(new Response(Response.OK, kvDao.get(request.getParameter(ID).getBytes())));
-
-            }
-
-            case Request.METHOD_PUT:{
-
-                kvDao.upsert(request.getParameter(ID).getBytes(), request.getBody());
+            case Request.METHOD_PUT:
+                kvDao.upsert(key.getBytes(), request.getBody());
                 session.sendResponse(new Response(Response.CREATED, Response.EMPTY));
+                break;
 
-            }
-
-            case Request.METHOD_DELETE:{
-
-                kvDao.remove(request.getParameter(ID).getBytes());
+            case Request.METHOD_DELETE:
+                kvDao.remove(key.getBytes());
                 session.sendResponse(new Response(Response.ACCEPTED, Response.EMPTY));
+                break;
 
-            }
-
-            default:{
-
+            default:
                 session.sendError(Response.BAD_REQUEST, null);
-            }
-
+                break;
         }
 
     }
